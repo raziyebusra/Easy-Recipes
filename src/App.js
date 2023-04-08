08ac667b9f750b53a93dd6c388967ee6382084cc
razyco <devrazyco@gmail.com>
2023-04-09 01:48:03 +0300

    added color theme for navbar

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Search from './pages/search/Search'
 import Recipe from './pages/recipe/Recipe'
+import ThemeSelector from './components/ThemeSelector'
 
 
 // styles
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <ThemeSelector />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/create" element={<Create /> } />  
